f914ad1f72a46cd406b0e50a6fe8dcf416f97721
Никита <popovn2268@gmail.com>
2024-10-09 12:40:44 +0300

    GG

--- PKMSite/WebSite/views.py
+++ PKMSite/WebSite/views.py
@@ -14,13 +14,12 @@ from django.shortcuts import redirect
 
 class CustomLoginView(LoginView):
     def form_valid(self, form):
-        # Получаем пользователя после успешного входа
+
         user = form.get_user()
 
-        # Проверяем имя пользователя
         if self.request.user.username == 'pkm':
             return redirect('/pkm/')
         elif self.request.user.get_username() == 'ntz':
             return redirect('/ntz/')
         else:
-            return super().form_valid(form)  # Стандартное поведение
+            return super().form_valid(form)
