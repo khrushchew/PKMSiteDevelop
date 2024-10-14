1ca24e128546f5fa72fcda61c17b0d7c9c705035
Никита <popovn2268@gmail.com>
2024-10-14 12:34:01 +0300

    ff

--- PKMSite/PKMSite/settings.py
+++ PKMSite/PKMSite/settings.py
@@ -20,17 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*&2b9#t#key&q&(9off#&d1k!tmlfy$40-q&5==xcbh4zdw$5('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
-'Идентификатор ключа YCAJEm-2S-mHduWCtZqItQD1G'
-
-'Ваш секретный ключ'
-'YCOrn0-JVO8Ale7lyFdEtKyg9ZPuOR-dkDCZEbzF'
 
 # Application definition
 
@@ -81,16 +76,7 @@ WSGI_APPLICATION = 'PKMSite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': 
-        {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'main_db',
-        'USER': 'postgres',
-        'PASSWORD': '12345678',
-        'HOST': 'localhost',
-        'PORT': '5432',},
-    }
+DATABASES = []
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
